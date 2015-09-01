7510ff05c38abd55e03e6cc4b400391936240146
Nicholas Dudfield <ndudfield@gmail.com>
2015-09-02 01:35:03 +0700

    Update transaction signing example

--- examples/sign-transaction.js
+++ examples/sign-transaction.js
@@ -3,38 +3,16 @@
 /* eslint-disable func-style */
 'use strict';
 
-const yargs = require('yargs');
+const path = require('path');
 const {STObject, binary} = require('../src');
 const {serializeObject, bytesToHex, sha512Half} = binary;
-const keyPairFromSeed = require('ripple-keypairs').keyPairFromSeed;
+const {keyPairFromSeed} = require('ripple-keypairs');
 
-const PREFIXES = {signing: [0x53, 0x54, 0x58, 0x00],
-                       id: [0x54, 0x58, 0x4E, 0x00]};
+const PREFIXES = {SIGNING: [0x53, 0x54, 0x58, 0x00],
+                       ID: [0x54, 0x58, 0x4E, 0x00]};
 
-const prettyJSON = obj => JSON.stringify(obj, undefined, 2);
-const signingData = tx => serializeObject(tx, PREFIXES.signing);
-
-function signTxJson(seed, tx_json) {
-  const keyPair = keyPairFromSeed(seed);
-  const tx = STObject.from(tx_json);
-
-  tx.SigningPubKey = bytesToHex(keyPair.pubKeyCanonicalBytes());
-  tx.TxnSignature = bytesToHex(keyPair.sign(signingData(tx)));
-
-  const serialized = serializeObject(tx);
-  const hash = bytesToHex(sha512Half(PREFIXES.id, serialized));
-  const tx_blob = bytesToHex(serialized);
-
-  return {
-    unsigned: tx_json,
-    signed: tx,
-    tx_blob,
-    hash
-  };
-}
-
-const example = {
-  seed: 'sEd7t79mzn2dwy3vvpvRmaaLbLhvme6',
+const EXAMPLE = {
+  secret: 'sEd7t79mzn2dwy3vvpvRmaaLbLhvme6',
   tx_json: JSON.stringify({
     Account: 'r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ',
     Amount: '1000',
@@ -46,12 +24,39 @@ const example = {
   })
 };
 
-(function main() {
-  const {argv: {_: [seed, tx_json]}} = yargs;
-  const params = {
-    seed: seed || example.seed,
-    tx_json: JSON.parse(tx_json || example.tx_json)
+const toHex = v => bytesToHex(v);
+const prettyJSON = obj => JSON.stringify(obj, undefined, 2);
+const signingData = tx => serializeObject(tx, PREFIXES.SIGNING);
+
+function signTxJson(secret, tx_json) {
+  const keyPair = keyPairFromSeed(secret);
+  // While we could just work directly off tx_json (try it) we create an
+  // STObject, so that many of the values are parsed only once.
+  const tx = STObject.from(tx_json);
+
+  tx.SigningPubKey = toHex(keyPair.pubKeyCanonicalBytes());
+  tx.TxnSignature = toHex(keyPair.sign(signingData(tx)));
+
+  const serialized = serializeObject(tx);
+  const hash = toHex(sha512Half(PREFIXES.ID, serialized));
+  const tx_blob = toHex(serialized);
+
+  return {
+    unsigned: tx_json,
+    signed: tx,
+    tx_blob,
+    hash
   };
-  const bundle = signTxJson(params.seed, params.tx_json);
-  console.log(prettyJSON(bundle));
+}
+
+(function main(args = process.argv) {
+  const [, script, secret, tx_json] = args;
+  if (args.length < 4) {
+    const relative = path.relative(process.cwd(), script);
+    console.error(`Usage: ${relative} <secret> <tx_json>\ne.g:`);
+    console.error(`${relative} ${EXAMPLE.secret} '${EXAMPLE.tx_json}'`);
+  } else {
+    const bundle = signTxJson(secret, JSON.parse(tx_json));
+    console.log(prettyJSON(bundle));
+  }
 }());
