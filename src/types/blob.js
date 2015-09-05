a18ce3fe32e072103020804268858c3622c4936c
Nicholas Dudfield <ndudfield@gmail.com>
2015-09-05 10:40:07 +0700

    Use SerializedType mixin for Blob

--- src/types/blob.js
+++ src/types/blob.js
@@ -2,17 +2,16 @@
 
 const makeClass = require('../make-class');
 const {parseBytes, bytesToHex} = require('../bytes-utils');
+const {SerializedType} = require('./serialized-type');
 
 const Blob = makeClass({
+  mixin: SerializedType,
   Blob(bytes) {
     if (bytes) {
       this._bytes = parseBytes(bytes, Uint8Array);
     } else {
       this._bytes = new Uint8Array(0);
     }
-  },
-  toBytesSink(sink) {
-    sink.put(this._bytes);
   },
   static: {
     fromParser(parser, hint) {
@@ -24,15 +23,6 @@ const Blob = makeClass({
       }
       return new this(value);
     }
-  },
-  toJSON() {
-    return this.toHex();
-  },
-  toHex() {
-    return bytesToHex(this._bytes);
-  },
-  toBytes() {
-    return this._bytes;
   }
 });
 
